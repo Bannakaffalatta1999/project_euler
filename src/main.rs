1a3cbec973b32ea6e1f13eb5f75fe0eac0419f79
Henry <flipit12321@gmail.com>
2023-01-08 20:15:29 +0100

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod problems;
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("Project euler!");
+
+    // Problem 1
+    println!("problem 1: {}", problems::prob1::p());
+
+    // Problem 2
+    println!("Problem 2: {}", problems::prob2::p());
+
+    // Problem 3
+    println!("Problem 3: {}", problems::prob3::p());
+
+    // Problem 4
+    println!("Problem 4: {}", problems::prob4::p());
+
+
+} 
